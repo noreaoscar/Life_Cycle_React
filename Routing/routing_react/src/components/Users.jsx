bd0e2fd0ba22eab8278c3015bb6280645043b8b4
Loreley López C <loreleylopezc@MacBook-Air-de-Loreley.local>
2023-02-08 18:38:35 -0600

    Cambios

--- Routing/routing_react/src/components/Users.jsx
+++ Routing/routing_react/src/components/Users.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Users extends React.Component{
     constructor(props){
         super(props)
         this.state ={
+            
             users:[]
         }
+       
     
     }
+    
 componentDidMount(){
     fetch("https://jsonplaceholder.typicode.com/users")
     .then(res=>res.json())
@@ -18,14 +22,19 @@ componentDidMount(){
         return(
             <>
             <h3>Users Components</h3>
-            
+
 
             {
                 this.state.users.map((user)=>{
                     return(
                         <div key={user.id}>
+                            <Link to="">
                             <h3>{user.name}</h3>
-                            <h4>{user.email}</h4>
+                            </Link>
+                           <Link to="">
+                           <h4>{user.email}</h4>
+                           </Link>
+                            
                             <hr /> 
                         </div>
                     )
